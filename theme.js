e1cfbcbe8d6bc6794d75d48018d019645499709a
awebdevguy <awebdevguy@gmail.com>
2021-10-06 22:43:04 -0600

    Update theme.js

--- theme.js
+++ theme.js
@@ -1,10 +1,14 @@
 let theme = localStorage.getItem('mode');
 
-if(theme === 'dark') {
-    document.body.classList.add("dark-mode");
-    // document.getElementById('profile-pic').setAttribute('src', "images/profile_banff.jpg");
-    document.getElementById('mode').innerText = 'Light';
-}
+// if(theme === 'dark') {
+    document.body.classList.add('dark-mode');
+    // document.body.classList.remove("light-mode");
+
+
+    // document.getElementById('mode').innerText = 'Light';
+// } else {
+  // document.getElementById('mode').innerText = 'Dark';
+// }
 
 // if(theme === 'sunny') {
 //     document.body.classList.add("bright-mode");
@@ -12,7 +16,11 @@ if(theme === 'dark') {
 //     document.getElementById('mode').innerText = 'Bland';
 // }
 
-if(theme === 'light') {
-    // document.getElementById('profile-pic').setAttribute('src', "images/profile_banff_bw.jpg");
-    document.getElementById('mode').innerText = 'Dark';
-}
+// if(theme === 'light') {
+//   // document.body.classList.add("light-mode");
+//   // document.body.classList.remove("dark-mode");
+//     // document.getElementById('profile-pic').setAttribute('src', "images/profile_banff_bw.jpg");
+//     document.getElementById('mode').innerText = 'Dark';
+// }
+
+
