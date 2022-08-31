19c9bf5e9a0a3f860afb88abf375e3f1dfc4f939
awebdevguy <awebdevguy@gmail.com>
2022-08-31 08:41:10 -0600

    change email function to formsubmit.co

--- functions.js
+++ functions.js
@@ -128,44 +128,43 @@ function readMore(index) {
   }
 
 //   Contact form send email
-window.addEventListener( "load", function () {
+// window.addEventListener( "load", function () {
 
-    function sendData() {
-        const XHR = new XMLHttpRequest();
+//     function sendData() {
+//         const XHR = new XMLHttpRequest();
   
-        // Bind the FormData object and the form element
-        const FD = new FormData( form );
+//         // Bind the FormData object and the form element
+//         const FD = new FormData( form );
     
-        // Define what happens on successful data submission
-        XHR.addEventListener( "load", function(event) {
-            if(event.target.responseText) {
-                alert( "Your email was sent successfully to Les, Thank you.");
-            }
-            resetContactForm();
-        } );
+//         // Define what happens on successful data submission
+//         XHR.addEventListener( "load", function(event) {
+//             if(event.target.responseText) {
+//                 alert( "Your email was sent successfully to Les, Thank you.");
+//             }
+//             resetContactForm();
+//         } );
     
-        // Define what happens in case of error
-        XHR.addEventListener( "error", function( event ) {
-            alert( 'Oops! Something went wrong.' );
-        } );
+//         // Define what happens in case of error
+//         XHR.addEventListener( "error", function( event ) {
+//             alert( 'Oops! Something went wrong.' );
+//         } );
     
-        // Set up our request
-        const proxyURL = 'https://quicors.herokuapp.com/'; // CORS
-        XHR.open( "POST", proxyURL + "https://script.google.com/macros/s/AKfycbzTN3_iPrRxlNFQArGG0IBDx5pZQvg-gkDDos3vTzsV90PfJp9O--FimoO72RJA04eY/exec");
+//         // Set up our request
+//         XHR.open( "POST", "https://script.google.com/macros/s/AKfycbzTN3_iPrRxlNFQArGG0IBDx5pZQvg-gkDDos3vTzsV90PfJp9O--FimoO72RJA04eY/exec");
     
-        // The data sent is what the user provided in the form
-        XHR.send( FD );
-    }
+//         // The data sent is what the user provided in the form
+//         XHR.send( FD );
+//     }
   
-    // Access the form element...
-    const form = document.getElementById( "contact-form" );
+//     // Access the form element...
+//     const form = document.getElementById( "contact-form" );
   
-    // ...and take over its submit event.
-    form.addEventListener( "submit", function ( event ) {
-      event.preventDefault();
-      sendData();
-    });
-});
+//     // ...and take over its submit event.
+//     form.addEventListener( "submit", function ( event ) {
+//       event.preventDefault();
+//       sendData();
+//     });
+// });
 
 function resetContactForm() {
 
